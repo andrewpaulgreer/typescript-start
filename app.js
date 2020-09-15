ca86281ace9ae5dae0f790765b2f64e0a1aa2656
Andrew Greer <62513150+andrewpaulgreer@users.noreply.github.com>
2020-09-15 14:14:38 -0400

    arrays in object

--- app.js
+++ app.js
@@ -4,6 +4,13 @@
 // } = {
 var person = {
     name: "andrew",
-    age: 20
+    age: 20,
+    hobbies: ['sports', 'cooking']
 };
+var favoriteActivities;
+favoriteActivities = ['sports'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -4,7 +4,15 @@
 // } = {
     const person = {
     name: "andrew",
-    age: 20
+    age: 20,
+    hobbies: ['sports', 'cooking']
 }
 
+let favoriteActivities: string[];
+favoriteActivities = ['sports']
+
 console.log(person.name)
+
+for (const hobby of person.hobbies){
+    console.log(hobby.toUpperCase())
+}
