9c202311f8a61d7a8951a63639b32672ab66ac39
Andrew Greer <62513150+andrewpaulgreer@users.noreply.github.com>
2020-09-15 14:23:28 -0400

    tuple added

--- app.ts
+++ app.ts
@@ -1,12 +1,24 @@
 // const person: {
 //     name: string,
 //     age: number
+
+import { string, number } from "prop-types"
+
 // } = {
-    const person = {
+    const person: {
+        name: string;
+        age: number;
+        hobbies: string[];
+        role: [number, string];
+    } = {
     name: "andrew",
     age: 20,
-    hobbies: ['sports', 'cooking']
-}
+    hobbies: ['sports', 'cooking'],
+    role: [2, 'author']
+};
+
+// person.role.push('admin')
+// person.role[1] = 10
 
 let favoriteActivities: string[];
 favoriteActivities = ['sports']
