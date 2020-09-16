d58df74e41c19e170a51dbbafaf46dfa4780ffdf
Andrew Greer <62513150+andrewpaulgreer@users.noreply.github.com>
2020-09-16 09:42:28 -0400

    union and literal types

--- app.ts
+++ app.ts
@@ -1,18 +1,27 @@
 // union types
 
-function combine(input1: number | string, input2: number | string){
-    // if issue with plus pops up it is not entirely correct
+function combine(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text'){
+   
     let result;
-    if(typeof input1 === 'number' && typeof input2 === 'number'){
-        result = input1 + input2
+    if(typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number'){
+        result = +input1 + +input2
     } else {
         result = input1.toString() + input2.toString()
     }
+    // if(resultConversion == 'as-number'){
+    //     return +result;
+    // } else {
+    //     return result.toString()
+    // }
     return result
 }
 
-const combinedAges = combine(30, 26)
+const combinedAges = combine(30, 26, 'as-number')
 console.log(combinedAges)
 
-const combinedNames = combine('Max', 'Anna')
+//as-number is a literal type
+const combinedStringAges = combine('30', '26', 'as-number')
+console.log(combinedStringAges)
+
+const combinedNames = combine('Max', 'Anna', 'as-text')
 console.log(combinedNames)
